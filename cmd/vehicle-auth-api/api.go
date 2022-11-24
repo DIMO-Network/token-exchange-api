48364814bf297151edf6f0213c3ab3b37ea538bb
0xdev22 <doyin@dimo.zone>
2022-11-24 09:45:15 -0500

    Return vehicle privilege token

--- cmd/vehicle-auth-api/api.go
+++ cmd/vehicle-auth-api/api.go
@@ -9,6 +9,7 @@ import (
 	"github.com/DIMO-Network/token-exchange-service/internal/api"
 	"github.com/DIMO-Network/token-exchange-service/internal/config"
 	vtx "github.com/DIMO-Network/token-exchange-service/internal/controllers"
+	"github.com/DIMO-Network/token-exchange-service/internal/services"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -19,7 +20,8 @@ import (
 
 func startWebAPI(ctx context.Context, logger zerolog.Logger, settings *config.Settings) {
 
-	vtxController := vtx.NewVehicleTokenExchangeController(&logger, settings)
+	dxS := services.NewDexService(&logger, settings)
+	vtxController := vtx.NewVehicleTokenExchangeController(&logger, settings, dxS)
 
 	app := fiber.New(fiber.Config{
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
--- internal/controllers/vehicle_token_exchange.go
+++ internal/controllers/vehicle_token_exchange.go
@@ -6,13 +6,14 @@ import (
 	"github.com/DIMO-Network/token-exchange-service/internal/api"
 	"github.com/DIMO-Network/token-exchange-service/internal/config"
 	"github.com/DIMO-Network/token-exchange-service/internal/contracts"
+	"github.com/DIMO-Network/token-exchange-service/internal/services"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
 
-func NewVehicleTokenExchangeController(logger *zerolog.Logger, settings *config.Settings) *VehicleTokenExchangeController {
+func NewVehicleTokenExchangeController(logger *zerolog.Logger, settings *config.Settings, dxS services.DexService) *VehicleTokenExchangeController {
 	client, err := ethclient.Dial(settings.BlockchainNodeUrl)
 	if err != nil {
 		logger.Fatal().Err(err).Str("blockchainUrl", settings.BlockchainNodeUrl).Msg("Failed to dial blockchain node.")
@@ -29,6 +30,7 @@ func NewVehicleTokenExchangeController(logger *zerolog.Logger, settings *config.
 		logger:           logger,
 		contractsManager: ctmr,
 		settings:         settings,
+		dexService:       dxS,
 	}
 }
 
@@ -36,6 +38,7 @@ type VehicleTokenExchangeController struct {
 	logger           *zerolog.Logger
 	contractsManager *contracts.ContractsManager
 	settings         *config.Settings
+	dexService       services.DexService
 }
 
 type VehiclePermissionRequest struct {
@@ -50,9 +53,14 @@ func (v *VehicleTokenExchangeController) GetVehicleCommandPermissionWithScope(c 
 		return fiber.NewError(fiber.StatusBadRequest, "Couldn't parse request body.")
 	}
 
+	if len(vpr.Privileges) == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "Please provide the privileges you need permission for.")
+	}
+
 	m := v.contractsManager.MultiPrivilege
 
 	hasPrivilege := true
+	token := ""
 	for _, p := range vpr.Privileges {
 		res, err := m.HasPrivilege(nil, vpr.VehicleTokenID, p, common.HexToAddress(vpr.UserAddress))
 		if err != nil {
@@ -65,7 +73,32 @@ func (v *VehicleTokenExchangeController) GetVehicleCommandPermissionWithScope(c 
 		}
 	}
 
-	return c.JSON(fiber.Map{
-		"hasPrivilege": hasPrivilege,
+	var p []int64
+	for _, v := range vpr.Privileges {
+		p = append(p, v.Int64())
+	}
+
+	if hasPrivilege {
+		tk, err := v.dexService.SignVehiclePrivilegePayload(c.Context(), services.VehiclePrivilegeDTO{
+			UserID:         vpr.UserAddress,
+			VehicleTokenID: vpr.VehicleTokenID.String(),
+			PrivilegeIDs:   p,
+		})
+
+		if err != nil {
+			return api.ErrorResponseHandler(c, err, fiber.StatusInternalServerError)
+		}
+
+		token = tk
+	}
+
+	resp := c
+
+	if !hasPrivilege {
+		resp = resp.Status(401)
+	}
+
+	return resp.JSON(fiber.Map{
+		"token": token,
 	})
 }
