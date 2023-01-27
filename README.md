7cf20c50e91550ece11e043ce51b57a74f0d7dcc
0xdev22 <doyin@dimo.zone>
2023-01-27 01:07:10 -0500

    Update swagger docs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Token Exchange API
 
 go run ./cmd/token-exchange-api
+
+## Run swagger
+
+swag init -g cmd/token-exchange-api/main.go --parseDependency --parseInternal --generatedTime true
--- internal/controllers/token_exchange.go
+++ internal/controllers/token_exchange.go
@@ -28,7 +28,7 @@ type PermissionTokenRequest struct {
 	// Privileges is a list of the desired privileges. It must not be empty.
 	Privileges []int64 `json:"privileges"`
 	// NFTContractAddress is the address of the NFT contract. Privileges will be checked
-	// on-chain at this address.
+	// on-chain at this address. Address must be in the 0x format e.g. 0x5FbDB2315678afecb367f032d93F642f64180aa3
 	NFTContractAddress string `json:"nftContractAddress"`
 }
 
