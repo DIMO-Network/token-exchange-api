a4d943957e21d5f36c063edb6d34d689a651fb5a
Allyson-English <allyson.english@gmail.com>
2025-05-13 12:32:00 -0400

    separate ipfs svc

--- internal/controllers/token_exchange.go
+++ internal/controllers/token_exchange.go
@@ -4,12 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"math/big"
-	"net/http"
-	"net/url"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/DIMO-Network/cloudevent"
@@ -23,6 +19,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
+
+//go:generate mockgen -source ./token_exchange.go -destination mocks/token_exchange_mock.go -package mock_controller_test
+type IPFSService interface {
+	Fetch(ctx context.Context, cid string) ([]byte, error)
+}
 
 var defaultAudience = []string{"dimo.zone"}
 
@@ -44,7 +45,7 @@ type TokenExchangeController struct {
 	dexService  services.DexService
 	ctmr        contracts.Manager
 	ethClient   bind.ContractBackend
-	ipfsBaseURL *url.URL
+	ipfsService IPFSService
 }
 
 type PermissionTokenRequest struct {
@@ -64,19 +65,16 @@ type PermissionTokenResponse struct {
 	Token string `json:"token"`
 }
 
-func NewTokenExchangeController(logger *zerolog.Logger, settings *config.Settings, dexService services.DexService,
+func NewTokenExchangeController(logger *zerolog.Logger, settings *config.Settings, dexService services.DexService, ipfsService IPFSService,
 	contractsMgr contracts.Manager, ethClient bind.ContractBackend) (*TokenExchangeController, error) {
-	ipfsBaseURL, err := url.Parse(settings.IPFSBaseURL)
-	if err != nil {
-		return nil, fmt.Errorf("invalid IPFS base URL: %w", err)
-	}
+
 	return &TokenExchangeController{
 		logger:      logger,
 		settings:    settings,
 		dexService:  dexService,
 		ctmr:        contractsMgr,
 		ethClient:   ethClient,
-		ipfsBaseURL: ipfsBaseURL,
+		ipfsService: ipfsService,
 	}, nil
 }
 
@@ -169,7 +167,7 @@ func (t *TokenExchangeController) createAndReturnToken(c *fiber.Ctx, pr *Permiss
 //   - *PermissionRecord: A pointer to the parsed permission record if valid, or nil if the document
 //     could not be fetched, parsed, or doesn't have the correct type
 func (t *TokenExchangeController) getValidSacdDoc(ctx context.Context, source string) (*models.PermissionRecord, error) {
-	sacdDoc, err := t.fetchFromIPFS(ctx, source)
+	sacdDoc, err := t.ipfsService.Fetch(ctx, source)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch JSON from IPFS: %w", err)
 	}
@@ -184,42 +182,6 @@ func (t *TokenExchangeController) getValidSacdDoc(ctx context.Context, source st
 	}
 
 	return &record, nil
-}
-
-// fetchFromIPFS retrieves content from IPFS using the provided content identifier (CID).
-// It constructs a URL using the configured IPFS base URL and the CID, then makes an HTTP GET
-// request to fetch the content.
-//
-// Parameters:
-//   - ctx: The context for the HTTP request, which can be used for cancellation and timeouts
-//   - cid: The IPFS content identifier, with or without the "ipfs://" prefix
-//
-// Returns:
-//   - []byte: The content retrieved from IPFS as a byte slice
-//   - error: An error if the request fails at any stage (URL construction, HTTP request creation,
-//     request execution, or response reading)
-func (t *TokenExchangeController) fetchFromIPFS(ctx context.Context, cid string) ([]byte, error) {
-	cid = strings.TrimPrefix(cid, "ipfs://")
-
-	ipfsURL := t.ipfsBaseURL.JoinPath(cid).String()
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ipfsURL, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to execute request: %w", err)
-	}
-	defer resp.Body.Close() //nolint:errcheck
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read IPFS response: %w", err)
-	}
-
-	return body, nil
 }
 
 // evaluateSacdDoc validates a SACD to determine if the requesting user has all the requested privileges.
---
+++ internal/services/ipfs_service.go
@@ -0,0 +1,79 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+var DefaultIPFSTimeout = 30
+
+const IPFS_PREFIX = "ipfs://"
+
+type IPFSClient struct {
+	logger      *zerolog.Logger
+	client      *http.Client
+	ipfsBaseURL *url.URL
+	timeout     time.Duration
+}
+
+func NewIPFSClient(logger *zerolog.Logger, ipfsBaseURL string, ipfsTimeout int) (*IPFSClient, error) {
+	ipfsURL, err := url.Parse(ipfsBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("invalid IPFS base URL: %w", err)
+	}
+
+	if ipfsTimeout == 0 {
+		ipfsTimeout = DefaultIPFSTimeout
+	}
+
+	return &IPFSClient{
+		logger:      logger,
+		client:      &http.Client{},
+		ipfsBaseURL: ipfsURL,
+		timeout:     time.Duration(ipfsTimeout) * time.Second,
+	}, nil
+}
+
+// FetchFromIPFS retrieves content from IPFS using the provided content identifier (CID).
+// It constructs a URL using the configured IPFS base URL and the CID, then makes an HTTP GET
+// request to fetch the content.
+//
+// Parameters:
+//   - ctx: The context for the HTTP request, which can be used for cancellation and timeouts
+//   - cid: The IPFS content identifier, with or without the "ipfs://" prefix
+//
+// Returns:
+//   - []byte: The content retrieved from IPFS as a byte slice
+//   - error: An error if the request fails at any stage (URL construction, HTTP request creation,
+//     request execution, or response reading)
+func (i *IPFSClient) Fetch(ctx context.Context, cid string) ([]byte, error) {
+	cid = strings.TrimPrefix(cid, IPFS_PREFIX)
+	ipfsURL := i.ipfsBaseURL.JoinPath(cid).String()
+
+	ctx, cancel := context.WithTimeout(ctx, i.timeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ipfsURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+
+	resp, err := i.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer resp.Body.Close() //nolint:errcheck
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("invalid ipfs status code: %d", resp.StatusCode)
+	}
+
+	return io.ReadAll(resp.Body)
+}
