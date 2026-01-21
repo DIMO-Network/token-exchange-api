f290f6b3ff4184b892619590c9bf7122e92562ce
Dylan Moreland <dylan@dimo.zone>
2026-01-21 12:59:14 -0500

    Some cleanup in the identity client

--- internal/services/identity/identity.go
+++ internal/services/identity/identity.go
@@ -19,31 +19,25 @@ type Client struct {
 	QueryEndpoint string
 }
 
-type QueryBody struct {
+type queryRequest struct {
 	Query     string         `json:"query"`
 	Variables map[string]any `json:"variables"`
 }
 
-type RawResponseSACD struct {
+type sacd struct {
 	Permissions string `json:"permissions"`
 	Source      string `json:"source"`
 }
 
-type Response struct {
-	IsOwner     bool
-	Permissions *big.Int
-	Source      string
-}
-
-type RawRespon struct {
+type queryResponse struct {
 	Data *struct {
-		Vehicle *Vehicle `json:"vehicle"`
+		Vehicle *vehicle `json:"vehicle"`
 	} `json:"data"`
 }
 
-type Vehicle struct {
-	Owner common.Address   `json:"owner"`
-	SACD  *RawResponseSACD `json:"sacd"`
+type vehicle struct {
+	Owner common.Address `json:"owner"`
+	SACD  *sacd          `json:"sacd"`
 }
 
 var query = `
@@ -58,6 +52,8 @@ query ($tokenId: Int!, $grantee: Address!) {
 }
 `
 
+// GetVehicleSACDSource returns the URI of the active SACD document for the given vehicle NFT and grantee.
+// If there is no active SACD set this will return an error, even if grantee is the owner of the vehicle.
 func (c *Client) GetVehicleSACDSource(ctx context.Context, tokenID int, grantee common.Address) (string, error) {
 	r, err := c.getVehicleSACD(ctx, tokenID, grantee)
 	if err != nil {
@@ -69,6 +65,8 @@ func (c *Client) GetVehicleSACDSource(ctx context.Context, tokenID int, grantee 
 	return r.SACD.Source, nil
 }
 
+// GetVehicleSACDPermissions returns the intersection of the provided permissions and the permissions the grantee has on
+// the given vehicle NFT. If grantee is the owner of the vehicle NFT then he will have all permissions.
 func (c *Client) GetVehicleSACDPermissions(ctx context.Context, tokenID int, grantee common.Address, permissions *big.Int) (*big.Int, error) {
 	r, err := c.getVehicleSACD(ctx, tokenID, grantee)
 	if err != nil {
@@ -88,13 +86,11 @@ func (c *Client) GetVehicleSACDPermissions(ctx context.Context, tokenID int, gra
 		return nil, err
 	}
 
-	fmt.Printf("ONCHAIN %s\n", onChainPerms.Text(16))
-
 	return new(big.Int).And(onChainPerms, permissions), nil
 }
 
-func (c *Client) getVehicleSACD(ctx context.Context, tokenID int, grantee common.Address) (*Vehicle, error) {
-	qb := QueryBody{
+func (c *Client) getVehicleSACD(ctx context.Context, tokenID int, grantee common.Address) (*vehicle, error) {
+	qb := queryRequest{
 		Query: query,
 		Variables: map[string]any{
 			"tokenId": tokenID,
@@ -129,7 +125,7 @@ func (c *Client) getVehicleSACD(ctx context.Context, tokenID int, grantee common
 		return nil, fmt.Errorf("status code %d", res.StatusCode)
 	}
 
-	var raw RawRespon
+	var raw queryResponse
 	err = json.Unmarshal(bb, &raw)
 	if err != nil {
 		return nil, err
